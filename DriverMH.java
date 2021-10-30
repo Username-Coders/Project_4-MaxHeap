dc59e83fe1e6c6e27c1635b0a8783f2dfb5f322d
JohnSalinas123 <salinasjohn257@gmail.com>
2021-10-30 00:03:03 -0700

    added driver file

---
+++ DriverMH.java
@@ -0,0 +1,24 @@
+public class DriverMH {
+
+     /**
+    * Reads 100 integers from a file, could be named "data.txt".
+    * @param fileName The name of the file where the integers are.
+    * @return Returns an integer array
+    */
+    public int[] read100Integers(String fileName) {
+        int[] result = new int[100];
+   
+
+
+        return result;
+    }
+
+
+    public static void main(String []args) {
+
+        MaxHeap<String> heap1 = new MaxHeap<>();
+
+
+    }
+    
+}
