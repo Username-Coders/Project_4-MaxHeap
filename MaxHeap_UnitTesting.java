f1030fc9194e9da10a65e0a342235d7a51b5dd08
JohnSalinas123 <salinasjohn257@gmail.com>
2021-11-07 20:33:43 -0800

    setup unit testing methods

---
+++ MaxHeap_UnitTesting.java
@@ -0,0 +1,61 @@
+import org.junit.Test;
+
+public class MaxHeap_UnitTesting {
+
+    @Test
+    public void sequentialHeapTest() {
+        
+    }
+
+    @Test
+    public void smartHeapTest() {
+
+    }
+
+    @Test
+    public void getMethodTest() {
+
+    }
+
+    @Test
+    public void getSwapsTest() {
+
+    }
+
+    @Test
+    public void readInputFileTest() {
+
+    }
+
+    @Test
+    public void printAfterCreationTest() {
+
+    }
+
+    @Test
+    public void printSwapsTest() {
+
+    }
+
+    @Test
+    public void perform10RemovalsTest() {
+
+    }
+
+    @Test
+    public void printAfterRemovalTest() {
+
+    }
+
+    @Test
+    public void printOutputBorderTest() {
+
+    }
+
+    @Test
+    public void makeFileTest() {
+        
+    }
+
+
+}
