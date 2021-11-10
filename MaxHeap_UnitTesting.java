d279c9a70d42493ef66174c00ffdd89a7458fcfe
JohnSalinas123 <salinasjohn257@gmail.com>
2021-11-09 22:50:10 -0800

    add complete unit tests

--- MaxHeap_UnitTesting.java
+++ MaxHeap_UnitTesting.java
@@ -4,10 +4,12 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Scanner;
 
 import org.junit.Test;
 
@@ -94,16 +96,99 @@ public class MaxHeap_UnitTesting {
     }
 
     @Test
-    public void readInputFileTest() throws IOException {
-        String testFile = "data_sorted.txt";
+    public void readInputFileTest() throws FileNotFoundException {
 
-        Integer[] result = DriverMH.read100Integers(testFile);
+        String fileName = "C:/Users/galax/git/CS 2400 Assignments/Project_4-MaxHeap/data_sorted.txt";
+        Integer[] result = DriverMH.read100Integers(fileName);
 
         Integer[] expected = new Integer[100];
-        for (int i = 0; i < 100; i++) {
-            expected[i] = Integer.valueOf(i + 10);
+        for (int i = 0; i < 100;i++) {
+            int tempInt = i + 1;
+            expected[i] = Integer.valueOf(tempInt);
         }
 
+        assertEquals(expected[0], result[0]);
+        
+
+    }
+
+    @Test
+    public void printAfterCreationTest() throws IOException {
+        String fileName = "testUnitTest.txt";
+        Integer[] heapValues = {20,40,30,10,90,70};
+
+        DriverMH.MakeAFile(fileName);
+
+        MaxHeap<Integer> smartHeap = new MaxHeap<>();
+        smartHeap.createHeap_SmartWay(heapValues);
+
+        MaxHeap<Integer> sequenHeap = new MaxHeap<>();
+        sequenHeap.createHeap_Sequential(heapValues);
+
+        DriverMH.print100Num_Creation(smartHeap, fileName);
+        DriverMH.print100Num_Creation(sequenHeap, fileName);
+
+        File myFile = new File(fileName);
+        Scanner outputFile = new Scanner(myFile);
+
+        
+        String readResult1 = outputFile.nextLine();
+        String readResult2 = outputFile.nextLine();
+
+        String expected1 = "Heap built using optimal method: 90,40,70,10,20,30,";
+        String expected2 = "Heap built using sequential insertions: 90,40,70,10,20,30,";
+        outputFile.close();
+        myFile.delete();
+
+        assertTrue(readResult1.equals(expected1));
+        assertTrue(readResult2.equals(expected2));
+
+
+    }
+
+    @Test
+    public void printSwapsTest() throws IOException {
+        String fileName = "testUnitTest.txt";
+        Integer[] heapValues = {20,40,30,10,90,70};
+
+        DriverMH.MakeAFile(fileName);
+
+        MaxHeap<Integer> smartHeap = new MaxHeap<>();
+        smartHeap.createHeap_SmartWay(heapValues);
+
+        DriverMH.printSwaps(smartHeap, fileName);
+
+        File myFile = new File(fileName);
+        Scanner outputFile = new Scanner(myFile);
+
+        String readResult = outputFile.nextLine();
+
+        String expected = "Number of swaps in the heap creation: 4";
+        outputFile.close();
+        myFile.delete();
+    
+        assertTrue(readResult.equals(expected));
+
+
+    }
+
+    @Test
+    public void perform10RemovalsTest() throws FileNotFoundException {
+
+        Integer[] heapValues = {20,40,30,1,10,90,70,50,60,70,10};
+
+        MaxHeap<Integer> heap = new MaxHeap<>();
+        heap.createHeap_SmartWay(heapValues);
+
+        DriverMH.perform10Removals(heap);
+
+        Integer[] result = new Integer[heap.getSize()];
+
+        for(int i = 1; i < heap.getSize() + 1; i++) {
+            result[i-1] = heap.getHeapValue_atIndex(i);
+        }
+
+        Integer[] expected = {1};
 
         assertArrayEquals(expected, result);
 
@@ -111,32 +196,69 @@ public class MaxHeap_UnitTesting {
     }
 
     @Test
-    public void printAfterCreationTest() {
+    public void printAfterRemovalTest() throws IOException {
+        
+        String fileName = "testUnitTest.txt";
+        Integer[] heapValues = {20,40,30,1,10,90,70,50,60,70,10};
+
+        DriverMH.MakeAFile(fileName);
+
+        MaxHeap<Integer> smartHeap = new MaxHeap<>();
+        smartHeap.createHeap_SmartWay(heapValues);
+
+        DriverMH.perform10Removals(smartHeap);
+        DriverMH.print100Num_Removal(smartHeap, fileName);
+
+        File myFile = new File(fileName);
+        Scanner outputFile = new Scanner(myFile);
+
+        
+        String readResult1 = outputFile.nextLine();
+
+        String expected1 = "Heap after 10 removals: 1,";
+
+        outputFile.close();
+        myFile.delete();
+
+        assertTrue(readResult1.equals(expected1));
 
     }
 
     @Test
-    public void printSwapsTest() {
+    public void printOutputBorderTest() throws IOException {
+
+        String fileName = "testUnitTest.txt";
+        Integer[] heapValues = {20,40,30,1,10,90,70,50,60,70,10};
+
+        DriverMH.MakeAFile(fileName);
+
+        DriverMH.printOutputBorder(fileName);
+
+        File myFile = new File(fileName);
+        Scanner outputFile = new Scanner(myFile);
+        
+        String readResult1 = outputFile.nextLine();
+
+        String expected1 = "=====================================================================";
+
+        outputFile.close();
+        myFile.delete();
+
+        assertTrue(readResult1.equals(expected1));
 
     }
 
     @Test
-    public void perform10RemovalsTest() {
+    public void makeFileTest() throws FileNotFoundException {
+        String fileName = "testUnitTest.txt";
+        DriverMH.MakeAFile(fileName);
 
-    }
+        File myFile = new File(fileName);
 
-    @Test
-    public void printAfterRemovalTest() {
+        boolean fileExists = myFile.exists();
+        myFile.delete();
 
-    }
-
-    @Test
-    public void printOutputBorderTest() {
-
-    }
-
-    @Test
-    public void makeFileTest() {
+        assertTrue(fileExists);
         
     }
 
